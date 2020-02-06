bf95e48e5465e0c25ad2b23933bc9f2c42bbdbbb
vagrant <vagrant@jenkins>
2020-02-06 11:56:25 +0000

    First Commit

---
+++ src/main/java/com/formation/simpleboot/service/Calculator.java
@@ -0,0 +1,12 @@
+package com.formation.simpleboot.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class Calculator {
+
+	public int sum(int a, int b) {
+		return a + b;
+	}
+}
